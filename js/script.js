e356305a2d43bfd1476bee4eaa0dbbc6b9487a86
Dawid Margraf <David_neo1@wp.pl>
2019-04-18 01:37:51 +0200

    Add carousel, restart button and scroll

---
+++ js/script.js
@@ -0,0 +1,19 @@
+(function () {
+    var elem = document.querySelector('.main-carousel');
+    var flkty = new Flickity(elem, {
+        cellAlign: 'left',
+        contain: true,
+        pageDots: false,
+        hash: true
+    });
+    document.querySelector('.btnRestart').addEventListener('click', function () {
+        flkty.select(0)
+    })
+    var progressBar = document.querySelector('.progress-bar')
+
+    flkty.on('scroll', function (progress) {
+        progress = Math.max(0, Math.min(1, progress));
+        progressBar.style.width = progress * 100 + '%';
+    });
+
+})()
