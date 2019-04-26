e0341891a006e3b1c6838f9019b8911d9c6c486f
Dawid Margraf <David_neo1@wp.pl>
2019-04-26 16:09:29 +0200

    Correct formating

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 (function () {
-    var carouselWrapper = document.querySelector('.main-carousel')
-  
+    var carouselWrapper = document.querySelector('.main-carousel');
     var templateSlide = document.getElementById('template-slide').innerHTML;
     Mustache.parse(templateSlide);
     var slidesHTML = ""
@@ -24,56 +23,45 @@
         progressBar.style.width = progress * 100 + '%';
     });
 	
-      var infos = document.getElementById('infos');
-        window.initMap = function() {
-          
-          var chiny = {lat: 36.111998, lng: 102.267816};
-          var grecja = {lat: 39.417295, lng: 22.588658};
-          var newYork = {lat: 40.714527,lng: -73.98648};
-          var rio = {lat: -22.906699, lng: -43.188875};
-          var rzym = {lat: 41.899275, lng: 12.478255};
-          var map = new google.maps.Map(document.getElementById('map'), {
-          
-              zoom: 7,
-              center: rzym
-          });
-      
-          var markerOne = new google.maps.Marker({
-              
-              position: rzym,            
-              map: map
-          }); 
-          markerOne.addListener('click', function(){
-			
-			infos.innerHTML = 'To jest Rzym';
+    var infos = document.getElementById('infos');
+    window.initMap = function() { 
+        var chiny = {lat: 36.111998, lng: 102.267816};
+        var grecja = {lat: 39.417295, lng: 22.588658};
+        var newYork = {lat: 40.714527,lng: -73.98648};
+        var rio = {lat: -22.906699, lng: -43.188875};
+        var rzym = {lat: 41.899275, lng: 12.478255};
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 7,
+            center: rzym
+        });
+        var markerOne = new google.maps.Marker({      
+            position: rzym,            
+            map: map
+        }); 
+        markerOne.addListener('click', function(){	
+        infos.innerHTML = 'To jest Rzym';
         });		
         var markerTwo = new google.maps.Marker({
-			position: chiny,
-			map: map
+            position: chiny,
+            map: map
         });
         markerTwo.addListener('click', function(){
-			
-			infos.innerHTML = 'To są Chiny';
+            
+            infos.innerHTML = 'To są Chiny';
         });		
         var markerThree = new google.maps.Marker({
-			position: newYork,
-			map: map
+            position: newYork,
+            map: map
         });
-        markerThree.addListener('click', function(){
-			
-			infos.innerHTML = 'To jest New York';
+        markerThree.addListener('click', function(){  
+            infos.innerHTML = 'To jest New York';
         });	
         document.getElementById('center-map').addEventListener('click', function(event){
-			event.preventDefault();
-	
-			map.panTo(newYork);
-		
-			map.setZoom(10);
+            event.preventDefault();
+            map.panTo(newYork);
+            map.setZoom(10);
         });	   
-    }
-       
-  })();  
-  
-  
-  initMap();
+    }       
+})();  
+
 
